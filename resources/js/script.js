6e4fa708c617d5887ee5b1938dc47434d43cbb5a
Seif <seif.dorani@gmail.com>
2018-05-02 18:34:05 -0700

    added jquery smooth scroll code snipet, and tested it on the food delivery gave it an achor of features and attached that id to the features section, will be doing the rest after this

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
   offset: '60px;'
   });
 
-  //scroll on Buttons
+//Scroll on Buttons
+
   //seclect the class and when we click on it
   //animation which scrolls to the top of the js section plan section at the speed of 1 second
   $('.js--scroll-to-plans').click(function() {
@@ -22,4 +23,43 @@ $(document).ready(function() {
 
   $('.js--scroll-to-start').click(function() {
       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+
+//Nav bar section clicks
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
   });
