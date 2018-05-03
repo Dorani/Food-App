8c35236f7968c1713c736ad8b1dbb90a84000e22
Seif <seif.dorani@gmail.com>
2018-05-02 18:17:20 -0700

    scroll affect in effect

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 //waypoint lib stuff:
   //nav bar sticky-ness during scroll event
-  //as soon as we hit the features section our nav bar appears 
+  //as soon as we hit the features section our nav bar appears
 $(document).ready(function() {
     $('.js--section-features').waypoint(function(direction){
       if (direction == 'down'){
@@ -11,4 +11,11 @@ $(document).ready(function() {
     })
 }, {
   offset: '60px;'
-});
+  });
+
+  //scroll on Buttons
+  //seclect the class and when we click on it
+  //animation which scrolls to the top of the js section plan section at the speed of 1 second
+  $('.js--scroll-to-plans').click(function() {
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
