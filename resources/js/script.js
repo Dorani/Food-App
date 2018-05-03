b2306c6b35ae9122f171563f1cd4422d92071ffb
Seif <seif.dorani@gmail.com>
2018-05-02 17:33:52 -0700

    notes on waypoint

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 //waypoint lib stuff:
-  //
+  //nav bar sticky-ness during scroll event
+  //as soon as we hit the features section our nav bar appears 
 $(document).ready(function() {
     $('.js--section-features').waypoint(function(direction){
       if (direction == 'down'){
