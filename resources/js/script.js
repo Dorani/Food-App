8c59a7f295fa76e476a02300e20583fd1c50ddb4
Seif <seif.dorani@gmail.com>
2018-05-02 17:27:51 -0700

    waypoint magic, have the nav bar get sticky when direction of the scroll is down, and remove it on way up

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,13 @@
+//waypoint lib stuff:
+  //
 $(document).ready(function() {
-
+    $('.js--section-features').waypoint(function(direction){
+      if (direction == 'down'){
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    })
+}, {
+  offset: '60px;'
 });
