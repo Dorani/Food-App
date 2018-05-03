318440c0a97bdcd40dee1b5259aa56743d814812
Seif <seif.dorani@gmail.com>
2018-05-02 21:46:59 -0700

    reverted..

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,47 +1,70 @@
+//waypoint lib stuff:
+  //nav bar sticky-ness during scroll event
+  //as soon as we hit the features section our nav bar appears
 $(document).ready(function() {
+    $('.js--section-features').waypoint(function(direction){
+      if (direction == 'down'){
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    })
+}, {
+  offset: '60px;'
+  });
+
+//Scroll on Buttons
+
+  //seclect the class and when we click on it
+  //animation which scrolls to the top of the js section plan section at the speed of 1 second
+  $('.js--scroll-to-plans').click(function() {
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function() {
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+  //Animations on Scroll:
+  $('.js-wp-1').waypoint(function(direction){
+      $('.js-wp-1').addClass('animated fadeIn');
+  });
 
 
-    /* For the sticky navigation */
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
-
-
-    /* Scroll on buttons */
-    $('.js--scroll-to-plans').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
-    });
-
-    $('.js--scroll-to-start').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-    });
-
-
-    /* Navigation scroll */
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
+//Nav bar section clicks
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
             return false;
-          }
-        }
-      });
-    });
-
-
-    /* Animations on scroll */
-    $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    });
-});
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
