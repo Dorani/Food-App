b67b8eb3976932dc47e09996053a8ca553643fd4
Seif <seif.dorani@gmail.com>
2018-05-04 13:42:37 -0700

    added 3 new waypoint event and their respective classes

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,28 +8,9 @@ $(document).ready(function() {
       } else {
         $('nav').removeClass('sticky');
       }
-    })
 }, {
   offset: '60px;'
   });
-
-//Scroll on Buttons
-
-  //seclect the class and when we click on it
-  //animation which scrolls to the top of the js section plan section at the speed of 1 second
-  $('.js--scroll-to-plans').click(function() {
-      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
-  });
-
-  $('.js--scroll-to-start').click(function() {
-      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-  });
-
-  //Animations on Scroll:
-  $('.js-wp-1').waypoint(function(direction){
-      $('.js-wp-1').addClass('animated fadeIn');
-  });
-
 
 //Nav bar section clicks
 // Select all links with hashes
@@ -67,4 +48,40 @@ $('a[href*="#"]')
         });
       }
     }
+
+    //Scroll on Buttons
+
+      //seclect the class and when we click on it
+      //animation which scrolls to the top of the js section plan section at the speed of 1 second
+      $('.js--scroll-to-plans').click(function() {
+          $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+      });
+
+      $('.js--scroll-to-start').click(function() {
+          $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+      });
+
+      //Animations on Scroll:
+      $('.js-wp-1').waypoint(function(direction){
+          $('.js-wp-1').addClass('animated fadeInUp');
+      }, {
+        offset: '50%'
+      });
+      $('.js-wp-2').waypoint(function(direction){
+          $('.js-wp-2').addClass('animated fadeIn');
+      }, {
+        offset: '50%'
+      });
+      $('.js-wp-3').waypoint(function(direction){
+          $('.js-wp-3').addClass('animated fadeInUp');
+      }, {
+        offset: '50%'
+      });
+      $('.js-wp-4').waypoint(function(direction){
+          $('.js-wp-4').addClass('animated pulse');
+      }, {
+        offset: '50%'
+      });
   });
+
+});
